933d1c221dda34b23f00e1f59e9d48b8f21c6c55
unknown <rijoanul.shanto@gmail.com>
2020-05-24 07:51:07 -0700

    dom element configured

--- static/js/app.js
+++ static/js/app.js
@@ -6,8 +6,18 @@ function loadData() {
     data.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let res = JSON.parse(this.responseText);
-            for (let contents of res) {
-                console.log(contents["topic"]);
+            
+            len = res.length;
+
+            for (let i = 0; i < len; i++) {
+                let liNode = document.createElement('li');
+                let liLink = document.createElement('a');
+                
+                liLink.setAttribute("herf", "#");
+                liLink.appendChild(document.createTextNode(res[i].topic));
+                
+                document.getElementById("topic").appendChild(liLink).setAttribute("class", "toctree-l1");
+                // console.log(i);
             }
         }
     };
